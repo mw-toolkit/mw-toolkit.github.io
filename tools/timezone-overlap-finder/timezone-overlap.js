e4d5d8aec7df21b71e92e52f33e4620652c0377e
Bimasakti1024 <bimasakti1024@proton.me>
2026-07-17 11:30:45 +0700

    renamed mwutil to mw-toolkit

--- tools/timezone-overlap-finder/timezone-overlap.js
+++
@@ -1,9 +0,0 @@
-import { toClipboard } from "/assets/js/util.js"
-
-document.getElementById("genbtn").addEventListener("click", () => {
-	document.getElementById("result").innerHTML = crypto.randomUUID();
-});
-
-document.getElementById("copy").addEventListener("click", () => {
-	toClipboard(document.getElementById("result").innerHTML);
-})
