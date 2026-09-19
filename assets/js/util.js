1db74134bd53b3ca4e226c62ee223e69050fa50a
Bimasakti1024 <bimasakti1024@proton.me>
2026-09-19 21:24:02 +0700

    chore: added getBMIcategory() to util.js

--- assets/js/util.js
+++ assets/js/util.js
@@ -13,6 +13,15 @@ export function rng(min, max) {
 	return Math.random() * (max - min + 1) + min;
 }
 
+export function getBMICategory(bmi) {
+	if (bmi < 18.5) return "Underweight";
+	if (bmi < 25) return "Normal weight";
+	if (bmi < 30) return "Overweight";
+	if (bmi < 35) return "Obese (Class I)";
+	if (bmi < 40) return "Obese (Class II)";
+	return "Obese (Class III)";
+}
+
 // Luhn algorithm
 export function luhn(num) {
 	let sum = 0;
