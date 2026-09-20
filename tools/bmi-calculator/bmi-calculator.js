dd03336461929f0d9d546b1c2a8c362b664265bc
Bimasakti1024 <bimasakti1024@proton.me>
2026-09-20 10:50:05 +0700

    chore(bmi-calculator): changed metric height from using meter to centimeter

--- tools/bmi-calculator/bmi-calculator.js
+++ tools/bmi-calculator/bmi-calculator.js
@@ -1,10 +1,12 @@
+import { getBMICategory } from "../../assets/js/util.js";
+
 const height_u_el = document.getElementById("height-unit");
 const weight_u_el = document.getElementById("weight-unit");
 const system_el = document.getElementById("system");
 
 function change_system() {
 	if (system_el.value == "metric") {
-		height_u_el.innerText = "m";
+		height_u_el.innerText = "cm";
 		weight_u_el.innerText = "kg";
 	} else {
 		height_u_el.innerText = "in";
@@ -13,4 +15,27 @@ function change_system() {
 }
 change_system();
 
-system_el.addEventListener("change", change_system)
+system_el.addEventListener("change", change_system);
+
+document.getElementById("calculate").addEventListener("click", () => {
+	const weight = document.getElementById("weight").value;
+	let height = document.getElementById("height").value;
+	let bmi;
+
+	/*
+		Imperial:
+			bmi = (w / h^2) * 703
+		Metric:
+			bmi = w /h
+	*/
+	if (system_el.value == "imperial") {
+		bmi = (weight / (height * height)) * 703;
+	} else {
+		height /= 100;
+		bmi = weight / (height * height);
+	}
+
+	const category = getBMICategory(bmi);
+
+	document.getElementById("result").innerText = `BMI: ${bmi}\nCategory: ${category}`;
+});
